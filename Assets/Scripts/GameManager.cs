8421bf0bf619fbb545f9b72b75d1ef3ef0a35289
aaaivan <ipalazzo.game@gmail.com>
2023-03-20 16:14:13 +0000

    Improved movement (better gravity)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,54 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public GameObject player;
+	public GameObject playerPrefab;
 	public CinemachineFreeLook cinemachine;
 	public Camera mainCamera;
 
-    void Start()
+	GameObject player;
+	public GameObject Player { get { return player; } }
+
+	static GameManager instance;
+	public static GameManager Instance { get { return instance; } }
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (instance == this)
+		{
+			instance = null;
+		}
+	}
+
+	void Start()
     {
 		// Generate terrain
         MapGenerator.Instance.DrawMapAtPosition(MapGenerator.Instance.transform.position);
 
 		// Spawn Player
 		Vector3 playerPos = MapGenerator.Instance.GetCoordinateOfNode(1, 1);
-		GameObject go = Instantiate(player, playerPos, Quaternion.identity, MapGenerator.Instance.transform);
-		cinemachine.Follow = go.transform.Find("CameraLookAt");
-		cinemachine.LookAt = go.transform.Find("CameraLookAt");
-		go.GetComponent<PlayerMovement>().camera = mainCamera.transform;
+		player = Instantiate(playerPrefab, playerPos, Quaternion.identity, MapGenerator.Instance.transform);
+		cinemachine.Follow = player.transform.Find("CameraLookAt");
+		cinemachine.LookAt = player.transform.Find("CameraLookAt");
+		player.GetComponent<PlayerMovement>().thirdPersonCamera = mainCamera.transform;
 
 		// Spawn Items
-		AssetsGeneratorManager.Instance.GenerateAssets(go.GetComponent<AStarAgent>());
+		AssetsGeneratorManager.Instance.GenerateAssets(player.GetComponent<AStarAgent>());
 	}
 }
